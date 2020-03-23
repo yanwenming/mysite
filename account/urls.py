59a7a54615bb5fd1f4f166e98e519c695d4627ea
yanwenming <ywm3685@qq.com>
2020-03-23 18:56:42 +0800

    upload

--- account/urls.py
+++ account/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('login/',auth_view.LoginView.as_view(template_name='account/login2.html'),name='user_login'),
     path('logout/',auth_view.LogoutView.as_view(template_name='account/logout.html'),name='user_logout'),
     path('register/',views.register,name='user_register'),
+    path('password-change/',auth_view.PasswordChangeView.as_view(template_name="account/password_change_form.html",success_url="/account/password-change-done/"),name='password_change'),
+    path('password-change-done/',auth_view.PasswordChangeDoneView.as_view(template_name="account/password_change_done.html"),name='password_change_done'),
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -142,3 +142,5 @@ STATICFILES_DIRS = (
 )
 
 LOGIN_REDIRECT_URL = '/blog/'
+
+LOGIN_URL = '/account/login/'
