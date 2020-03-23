6134b3743f37cb7752a751af80541af7a2e0e26a
yanwenming <ywm3685@qq.com>
2020-03-23 16:38:36 +0800

    upload

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_view
 
 app_name="account"
 urlpatterns = [
-    path('login/',views.user_login,name='user_login'),
+    # path('login/',views.user_login,name='user_login'),
+    path('login/',auth_view.LoginView.as_view(template_name='account/login2.html'),name='user_login'),
+    path('logout/',auth_view.LogoutView.as_view(template_name='account/logout.html'),name='user_logout'),
+    path('register/',views.register,name='user_register'),
 ]
