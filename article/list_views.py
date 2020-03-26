e089ab5d90a0bc7e0e85e0148a4faa4f152da05e
yanwenming <ywm3685@qq.com>
2020-03-26 19:46:35 +0800

    uploae

--- article/list_views.py
+++ article/list_views.py
@@ -7,23 +7,31 @@ from django.contrib.auth.models import User
 
 def article_titles(request,username=None):
     if username:
-        user = User.objects.get(username=username)
-        articles_title = ArticlePost.objects.filter(author=user)
-    else:
+        user = User.objects.get(username = username)
+        articles_title = ArticlePost.objects.filter(author = user)
+        try:
+            userinfo = user.userinfo
+        except:
+            userinfo = None
+    else :
         articles_title = ArticlePost.objects.all()
-    # article_title = ArticlePost.objects.all()
-    paginator = Paginator(articles_title,2)
+        # articles_title = ArticlePost.objects.all()
+    paginator = Paginator(articles_title , 2)
     page = request.GET.get('page')
-    try:
+    try :
         current_page = paginator.page(page)
         articles = current_page.object_list
-    except PageNotAnInteger:
+    except PageNotAnInteger :
         current_page = paginator.page(1)
         articles = current_page.object_list
-    except EmptyPage:
+    except EmptyPage :
         current_page = paginator.page(paginator.num_pages)
         articles = current_page.object_list
-    return render(request,"article/list/article_titles.html",{"articles":articles,"page":current_page})
+
+    if username :
+        return render(request, "article/list/author_articles.html",
+                        {"articles": articles , "page": current_page, "userinfo": userinfo, "user": user})
+    return render(request , "article/list/article_titles.html", {"articles": articles, "page": current_page})
 
 
 def article_detail(request,id,slug):
