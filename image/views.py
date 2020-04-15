4c0f05d000b22068fcab17ffc01860786af67479
yanwenming <ywm3685@qq.com>
2020-04-15 14:24:58 +0800

    缩略图

--- image/views.py
+++ image/views.py
@@ -5,6 +5,8 @@ from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
 from .forms import ImageForm
 from .models import Image
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+
 
 #上传图片视图函数
 @login_required(login_url='/account/login/')
@@ -25,7 +27,21 @@ def upload_image(request):
 @login_required(login_url='/account/login/')
 def list_images(request):
     images = Image.objects.filter(user=request.user)
-    return render(request, 'image/list_images.html', {"images": images})
+
+    #实现分页功能
+    paginator = Paginator(images,5)
+    page = request.GET.get('page')
+    try :
+        current_page = paginator.page(page)
+        images_list = current_page.object_list
+    except PageNotAnInteger :
+        current_page = paginator.page(1)
+        images_list = current_page.object_list
+    except EmptyPage :
+        current_page = paginator.page(paginator.num_pages)
+        images_list = current_page.object_list
+
+    return render(request, 'image/list_images.html', {"images": images_list,"page": current_page})
 
 
 #删除图片视图函数
